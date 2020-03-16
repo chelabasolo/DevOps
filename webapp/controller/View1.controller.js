9fb521adc7c564209eaf97e4eb7ea1ef93595ebc
Rachel Joy abasolo <chelabasolo03@gmail.com>
2020-03-16 03:23:20 +0000

    initial

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("TestApp3.TestApp3.controller.View1", {
+		onInit: function () {
+
+		}
+	});
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"TestApp3/TestApp3/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
